ac783f081698e58f2fdf46a9479544176e9b4409
Justin Villanueva <VIL22528@spengergasse.at>
2024-11-22 00:09:09 +0100

    added atlas cloud

--- backend/src/main/java/at/spengergasse/backend/BackendApplication.java
+++ backend/src/main/java/at/spengergasse/backend/BackendApplication.java
@@ -1,13 +1,37 @@
 package at.spengergasse.backend;
 
+import com.mongodb.*;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import org.bson.Document;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+import static com.mongodb.client.model.Filters.eq;
 
 @SpringBootApplication
 public class BackendApplication {
 
 	public static void main(String[] args) {
+
+		// Replace the placeholder with your MongoDB deployment's connection string
+		String uri = "mongodb+srv://clemil:emil@clemil0.wbzbs.mongodb.net/?retryWrites=true&w=majority&appName=Clemil0";
+
+		try (MongoClient mongoClient = MongoClients.create(uri)) {
+			MongoDatabase database = mongoClient.getDatabase("sample_mflix");
+			MongoCollection<org.bson.Document> collection = database.getCollection("movies");
+
+			Document doc = collection.find(eq("title", "Back to the Future")).first();
+			if (doc != null) {
+				System.out.println(doc.toJson());
+			} else {
+				System.out.println("No matching documents found.");
+			}
+		}
+
 		SpringApplication.run(BackendApplication.class, args);
 	}
 
--- backend/src/main/java/at/spengergasse/backend/config/MongoConfig.java
+++ backend/src/main/java/at/spengergasse/backend/config/MongoConfig.java
@@ -1,10 +1,33 @@
 package at.spengergasse.backend.config;
 
+import com.mongodb.ConnectionString;
+import com.mongodb.MongoClientSettings;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import org.bson.UuidRepresentation;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @Configuration
 @EnableMongoRepositories(
         basePackages = {"at.spengergasse.backend.mongodb.persistenceRef", "at.spengergasse.backend.mongodb.persistence"}
 )
-public class MongoConfig { }
+public class MongoConfig {
+
+    @Bean
+    public MongoClient mongoClient() {
+        MongoClientSettings settings = MongoClientSettings.builder()
+                .uuidRepresentation(UuidRepresentation.STANDARD)
+                .applyConnectionString(new ConnectionString("mongodb+srv://clemil:emil@clemil0.wbzbs.mongodb.net/?retryWrites=true&w=majority"))
+                .build();
+
+        return MongoClients.create(settings);
+    }
+
+    @Bean
+    public MongoTemplate mongoTemplate(MongoClient mongoClient) {
+        return new MongoTemplate(mongoClient, "dbi");
+    }
+}
